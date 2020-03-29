addf4a4b2de17e875047f12dccff79a2b2490819
netkas <netkas@intellivoid.info>
2020-03-29 00:44:08 -0300

    Added section 'javascript'

---
+++ src/resources/sections/javascript.php
@@ -0,0 +1,10 @@
+<script src="/assets/plugins/jquery/jquery.min.js"></script>
+<script src="/assets/plugins/bootstrap/js/popper.min.js"></script>
+<script src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="/assets/js/jquery.slimscroll.js"></script>
+<script src="/assets/js/waves.js"></script>
+<script src="/assets/js/sidebarmenu.js"></script>
+<script src="/assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+<script src="/assets/plugins/sparkline/jquery.sparkline.min.js"></script>
+<script src="/assets/js/custom.min.js"></script>
+<script src="/assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
