d379d5876ab06d01ad27ec26972f6f214d12765b
netkas <netkas@intellivoid.info>
2020-03-31 22:05:15 -0300

    Added 'create_session'

--- src/resources/pages/v1_coffeehouse_lydia_create_session/contents.php
+++ src/resources/pages/v1_coffeehouse_lydia_create_session/contents.php
@@ -35,6 +35,64 @@
                             <div class="card">
                                 <div class="card-body">
                                     <h3 class="card-title">Creating a session</h3>
+                                    at the beginning of each conversation with each user or group a session must be
+                                    established, this session must not change throughout the conversation unless it has
+                                    expired or is no longer available due to an AI Error.
+                                    <br/>
+                                    <br/>
+                                    If a new session ID gets established for each incoming message, this will result in
+                                    weird conversations that will not make sense since the AI will not be able to recall
+                                    the last few messages of the conversation with the user or group
+                                    <br/>
+                                    <br/>
+                                    This method will return a Session Object upon request
+                                    <hr/>
+                                    <div id="parameters">
+                                        <div class="alert alert-danger">
+                                            <h3 class="text-danger">
+                                                <i class="fa fa-exclamation-circle"></i> Session Creation Limit
+                                            </h3>
+                                            Depending on your subscription, there are a limited amount of sessions that
+                                            you can create. Each session lasts around for 3 hours but the usage for
+                                            each session contains no limit.
+                                            <br/>
+                                            <br/>
+                                            For example, your subscription may only support "100 Monthly Lydia Sessions"
+                                            which means that you can only create 100 sessions per month and that limitation
+                                            only resets after a billing cycle (Both paid and free)
+                                        </div>
+                                        <?PHP
+                                            $create_session_parameters_path = __DIR__ . DIRECTORY_SEPARATOR . 'create_session_parameters.json';
+                                            $create_session_parameters = json_decode(file_get_contents($create_session_parameters_path), true);
+                                        ?>
+                                        <label for="auth_example" class="mt-2">Endpoint</label>
+                                        <input class="form-control <?PHP theme_TextColor(); ?> mb-3" id="auth_example" value="<?PHP HTML::print($create_session_parameters['ENDPOINT']); ?>" readonly>
+                                        <label>Parameters</label>
+                                        <?PHP
+                                            generate_parameters_table($create_session_parameters);
+                                        ?>
+                                    </div>
+                                    <hr/>
+                                    <div id="response">
+                                        <h4>Example Success Response</h4>
+                                        <pre><code class="language-json"><?PHP HTML::print(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR  . 'create_session_success.json'), true); ?></code></pre>
+                                    </div>
+                                    <br/>
+                                    <div class="session_structure">
+                                        <h4>Session Object Structure</h4>
+                                        <?PHP
+                                            $api_methods_path = __DIR__ . DIRECTORY_SEPARATOR . 'session_object_structure.json';
+                                            generate_object_structure(json_decode(file_get_contents($api_methods_path), true));
+                                        ?>
+                                    </div>
+                                    <hr/>
+                                    <div id="invalid_language_code">
+                                        <h4>Invalid Language Code Response</h4>
+                                        <br/>
+                                        This response is given when the parameter <code>target_language</code> contains
+                                        an invalid value.
+                                        <pre><code class="language-json"><?PHP HTML::print(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR  . 'invalid_language_code.json'), true); ?></code></pre>
+                                    </div>
                                 </div>
                             </div>
 
--- src/resources/runtime_scripts/data_generator.php
+++ src/resources/runtime_scripts/data_generator.php
@@ -1,12 +1,12 @@
 <?php
 
-use DynamicalWeb\DynamicalWeb;
-use DynamicalWeb\HTML;
+    use DynamicalWeb\DynamicalWeb;
+    use DynamicalWeb\HTML;
 
     function generate_methods_table(array $data)
     {
         HTML::print("<div class=\"table-responsive\">", false);
-        HTML::print("<table class=\"table table-hover\">", false);
+        HTML::print("<table class=\"table table-hover table-bordered\">", false);
 
         $Headers = [
             "Method Name",
@@ -29,34 +29,147 @@ use DynamicalWeb\HTML;
         {
             HTML::print("<tr>", false);
 
-            HTML::print("<th>", false);
+            HTML::print("<td>", false);
             HTML::print("<a href=\"", false);
             HTML::print(DynamicalWeb::getRoute($method['REFERENCE']), true);
             HTML::print("\">", false);
             HTML::print($method['METHOD_NAME'], true);
             HTML::print("</a>", false);
-            HTML::print("</th>", false);
+            HTML::print("</td>", false);
 
-            HTML::print("<th>", false);
+            HTML::print("<td>", false);
             HTML::print("<a target=\"_blank\" href=\"", false);
             HTML::print($data['ENDPOINT'] . $method['ACCESS_URI'], true);
             HTML::print("\">", false);
             HTML::print($method['ACCESS_URI'], true);
             HTML::print("</a>", false);
-            HTML::print("</th>", false);
+            HTML::print("</td>", false);
 
-            HTML::print("<th>", false);
+            HTML::print("<td>", false);
             foreach($method['REQUEST_METHODS'] as $request_method)
             {
                 HTML::print("<code class=\"mr-1\">", false);
                 HTML::print($request_method, true);
                 HTML::print("</code>", false);
             }
-            HTML::print("</th>", false);
+            HTML::print("</td>", false);
 
-            HTML::print("<th>", false);
+            HTML::print("<td>", false);
             HTML::print($method['DESCRIPTION'], true);
-            HTML::print("</th>", false);
+            HTML::print("</td>", false);
+
+            HTML::print("</tr>", false);
+        }
+        HTML::print("</tbody>", false);
+
+        HTML::print("</table>", false);
+        HTML::print("</div>", false);
+    }
+
+    function generate_parameters_table(array $data)
+    {
+        HTML::print("<div class=\"table-responsive\">", false);
+        HTML::print("<table class=\"table table-hover table-bordered\">", false);
+
+        $Headers = [
+            "Parameter Name",
+            "Default Value",
+            "Required",
+            "Description"
+        ];
+
+        HTML::print("<thead><tr>", false);
+        foreach($Headers as $header)
+        {
+            HTML::print("<td>", false);
+            HTML::print($header, true);
+            HTML::print("</td>", false);
+        }
+        HTML::print("</tr></thead>", false);
+
+        HTML::print("<tbody>", false);
+        foreach($data['PARAMETERS'] as $parameter)
+        {
+            HTML::print("<tr>", false);
+
+            HTML::print("<td>", false);
+            HTML::print($parameter['PARAMETER_NAME'], true);
+            HTML::print("</td>", false);
+
+            HTML::print("<td>", false);
+            if(is_null($parameter['DEFAULT_VALUE']))
+            {
+                HTML::print("<code>", false);
+                HTML::print("NULL", true);
+                HTML::print("</code>", false);
+            }
+            else
+            {
+                HTML::print($parameter['DEFAULT_VALUE'], true);
+            }
+            HTML::print("</td>", false);
+
+            HTML::print("<td>", false);
+            if($parameter['REQUIRED'])
+            {
+                HTML::print("True");
+            }
+            else
+            {
+                HTML::print("False");
+            }
+            HTML::print("</td>", false);
+
+            HTML::print("<td>", false);
+            HTML::print($parameter['DESCRIPTION'], true);
+            HTML::print("</td>", false);
+
+            HTML::print("</tr>", false);
+        }
+        HTML::print("</tbody>", false);
+
+        HTML::print("</table>", false);
+        HTML::print("</div>", false);
+    }
+
+    function generate_object_structure(array $data)
+    {
+        HTML::print("<div class=\"table-responsive\">", false);
+        HTML::print("<table class=\"table table-hover table-bordered\">", false);
+
+        $Headers = [
+            "Name",
+            "Type",
+            "Description"
+        ];
+
+        HTML::print("<thead><tr>", false);
+        foreach($Headers as $header)
+        {
+            HTML::print("<td>", false);
+            HTML::print($header, true);
+            HTML::print("</td>", false);
+        }
+        HTML::print("</tr></thead>", false);
+
+        HTML::print("<tbody>", false);
+        foreach($data['VARIABLES'] as $variable)
+        {
+            HTML::print("<tr>", false);
+
+            HTML::print("<td>", false);
+            HTML::print($variable['NAME'], true);
+            HTML::print("</td>", false);
+
+            HTML::print("<td>", false);
+            HTML::print("<code>", false);
+            HTML::print($variable['TYPE'], true);
+            HTML::print("</code>", false);
+            HTML::print("</td>", false);
+
+            HTML::print("<td>", false);
+            HTML::print($variable['DESCRIPTION'], true);
+            HTML::print("</td>", false);
 
             HTML::print("</tr>", false);
         }
